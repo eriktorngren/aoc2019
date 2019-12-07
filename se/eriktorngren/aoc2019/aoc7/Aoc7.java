77c2cec865387cfc598aaf925ff23f7af5bce0bc
Erik Törngren <erito014@student.liu.se>
2019-12-07 17:34:48 +0100

    day 7 - day 7.5

--- se/eriktorngren/aoc2019/aoc7/Aoc7.java
+++ se/eriktorngren/aoc2019/aoc7/Aoc7.java
@@ -1,4 +1,4 @@
-package se.eriktorngren.aoc2019.aoc5;
+package se.eriktorngren.aoc2019.aoc7;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -6,13 +6,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
-public class Aoc5 {
+public class Aoc7 {
 
 	public static void main(String[] args) {
 		List<String> records = null;
-		try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Erik\\Documents\\aoc2019\\aoc2019\\se\\eriktorngren\\aoc2019\\aoc5\\input.txt"))) {
+		try (BufferedReader br = new BufferedReader(new FileReader("/Users/Erik/Documents/AOC2019/aoc2019/se/eriktorngren/aoc2019/aoc7/input.txt"))) {
 			String line;
 			while ((line = br.readLine()) != null) {
 				String[] values = line.split(",");
@@ -23,11 +22,50 @@ public class Aoc5 {
 		}
 		List<Integer> computer = new ArrayList<Integer>();
 		for(String s : records) computer.add(Integer.valueOf(s));
-		runSequence(computer);
-		System.out.println("Program finished");
+		System.out.println(computer);
+
+		for(String s : records) computer.add(Integer.valueOf(s));
+		List<Integer> phase = new ArrayList<Integer>();
+		for (int i=0; i<5; i++) {
+			phase.add(i);
+		}
+		List<List<Integer>> phaseSettings = generatePerm(phase);
+
+		System.out.println(phaseSettings);
+		System.out.println(phaseSettings.size());
+		int highestOutput = 0;
+		for (int i = 0; i<phaseSettings.size(); i++) {
+			int output = 0;
+			for (int setting : phaseSettings.get(i)) {
+				output = runSequence(computer, setting, output);
+			}
+			highestOutput = output > highestOutput ? output : highestOutput;
+		}
+		System.out.println(highestOutput);
 	}
 
-	public static void runSequence(List<Integer> computer) {
+	//4 43 432 4321
+	public static List<List<Integer>> generatePerm(List<Integer> original) {
+		if (original.size() == 0) {
+			List<List<Integer>> result = new ArrayList<List<Integer>>(); 
+			result.add(new ArrayList<Integer>()); 
+			return result; 
+		}
+		Integer firstElement = original.remove(0);
+		List<List<Integer>> returnValue = new ArrayList<List<Integer>>();
+		List<List<Integer>> permutations = generatePerm(original);
+		for (List<Integer> smallerPermutated : permutations) {
+			for (int index=0; index <= smallerPermutated.size(); index++) {
+				List<Integer> temp = new ArrayList<Integer>(smallerPermutated);
+				temp.add(index, firstElement);
+				returnValue.add(temp);
+			}
+		}
+		return returnValue;
+	}
+
+	public static int runSequence(List<Integer> computer, int setting, int output) {
+		int switcher = 0;
 		int i = 0;
 		String current = computer.get(0).toString();
 		int opcode = 0;
@@ -39,7 +77,6 @@ public class Aoc5 {
 		int second;
 		int index;
 
-		Scanner in = new Scanner(System.in);
 		while (Integer.valueOf(current) != 99) {
 			String modes = String.format("%05d", Integer.valueOf(current));
 			opcode = Integer.parseInt(modes.substring(3));
@@ -60,11 +97,14 @@ public class Aoc5 {
 				computer.set(index, firstGet*secondGet);
 				i += 4;
 			} else if (opcode == 3) {
-				System.out.println("Give input:");
-				computer.set(first, Integer.valueOf(in.nextLine()));
+				int input = switcher == 0 ? setting : output;
+				switcher = 1;
+				//System.out.println("Input: " + input);
+				computer.set(first, input);
 				i += 2;
 			} else if (opcode == 4) {
-				System.out.println("Output: " + firstGet);
+				//System.out.println("Output: " + firstGet);
+				output= firstGet;
 				i += 2;
 			} else if (opcode == 5) {
 				if (firstGet != 0) {
@@ -103,5 +143,7 @@ public class Aoc5 {
 			}
 			current =  computer.get(i).toString();
 		}
+		return output;
 	}
+
 }
