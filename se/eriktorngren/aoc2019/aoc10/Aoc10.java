98335eb041d121c91cd2c040e70270a1f479fbc0
Erik Törngren <erito014@student.liu.se>
2019-12-10 22:58:42 +0100

    completed day 10 part 1

--- se/eriktorngren/aoc2019/aoc10/Aoc10.java
+++ se/eriktorngren/aoc2019/aoc10/Aoc10.java
@@ -3,16 +3,19 @@ package se.eriktorngren.aoc2019.aoc10;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Aoc10 {
 
 	static int xMax;
 	static int yMax;
 	static List<List<String>> map;
-	
+
 	public static void main(String[] args) {
 		map = new ArrayList<>();
 		try (BufferedReader br = new BufferedReader(new FileReader("/Users/Erik/Documents/AOC2019/aoc2019/se/eriktorngren/aoc2019/aoc10/input.txt"))) {
@@ -30,50 +33,46 @@ public class Aoc10 {
 
 		List<List<Integer>> asteroids = new ArrayList<>();
 
-		for (int x = 0; x<xMax; x++) {
-			for (int y = 0; y<yMax; y++) {
+		for (int y = 0; y<xMax; y++) {
+			for (int x = 0; x<yMax; x++) {
 				if (map.get(x).get(y).equals("#")) {
 					List<Integer> asteroid = new ArrayList<>();
-					asteroid.add(x);
 					asteroid.add(y);
+					asteroid.add(x);
 					asteroids.add(asteroid);
 				}
 			}
 		}
-
+		int best = 0;
 		System.out.println(asteroids);
 		for (List<Integer> asteroid : asteroids) {
-			int lineOfSight = detect(asteroid);
-			System.out.println(lineOfSight);
+			int lineOfSight = detect(asteroid, asteroids);
+			best = best > detect(asteroid, asteroids) ? best : detect(asteroid, asteroids);
 		}
-
+		System.out.println(best);
 	}
 
-	private static int detect(List<Integer> currentAsteroid) {
-		int detected = 0;
-		for (int x = -xMax; x<xMax; x++) {
-			for (int y = -yMax; y<yMax; y++) {
-				if (visible(x, y, currentAsteroid)) {
-					detected++;
-				}
+	private static int detect(List<Integer> currentAsteroid, List<List<Integer>> asteroids) {
+		//int detected = 0;
+		int currentX = currentAsteroid.get(0);
+		int currentY = currentAsteroid.get(1);
+		Map<List<Integer>, Boolean> test = new HashMap<>();
+		for (List<Integer> asteroid : asteroids) {
+			List<Integer> detected = new ArrayList<>();
+			int x = asteroid.get(0);
+			int y = asteroid.get(1);
+			int gcd = BigInteger.valueOf(currentX-x).gcd(BigInteger.valueOf(currentY-y)).intValue();
+
+			if (gcd != 0) {
+				detected.add((currentX-x)/gcd);
+				detected.add((currentY-y)/gcd);
+			} else {
+				detected.add(currentX-x);
+				detected.add(currentY-y);
 			}
+			test.put(detected, true);
 		}
-		return detected;
-	}
-
-	private static boolean visible(int x, int y, List<Integer> currentAsteroid) {
-		int currentX = currentAsteroid.get(0) + x;
-		int currentY = currentAsteroid.get(1) + y;
-		
-		if (currentX < 0 || currentX>=xMax || currentY < 0 || currentY>=yMax) {
-			return false;
-		}
-		
-		if (map.get(currentX).get(currentY).equals("#")) {
-			return true;
-		} else {
-			return visible(currentX, currentY, currentAsteroid);
-		}
+		return test.size()-1;
 	}
 
 
